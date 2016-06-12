dbd511c6a0a819d9feb22ba2a94cbde7807dbbe1
pengduo <wang@pengduo.com.cn>
2016-06-12 17:31:41 +0800

    ...

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 var varConfig = {
-    "version": 5,
-    "datetime": "2016-06-09 12:14:44",
-    "monitorgroups": []
-};
+        "version": 5,
+        "datetime": "2016-06-12 15:17:59",
+        "monitorgroups": []
+    }
+    ;
