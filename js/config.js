0d553adfcfe6125c83d23b768dca8b886c17650a
pengduo <wang@pengduo.com.cn>
2016-06-23 12:31:53 +0800

    完善了业务组的可用性告警，增加了真实数组及提示和链接跳转

--- js/config.js
+++ js/config.js
@@ -1,1 +1,34 @@
-var varConfig = { "version": 5, "datetime": "2016-06-16 11:32:36", "monitorgroups": [ { "name": "Applications Manager", "resourceid": "10000011", "details": [ { "name": "192.168.0.105", "type": "Port-Test", "x": 723.9447665050427, "y": 152.07404116798463, "rel": [] }, { "name": "192.168.0.105_PGSQL", "type": "PostgreSQL", "x": 560.8792952988382, "y": 267.8750004997413, "rel": [ "192.168.0.105", "pt_Windows 7" ] }, { "name": "192.168.0.105_Tomcat-server", "type": "Tomcat-server", "x": 360.93765333757346, "y": 263.03965864639116, "rel": [ "192.168.0.105_PGSQL" ] }, { "name": "AppManager Home Page", "type": "UrlMonitor", "x": 160.9440912896548, "y": 261.43489534509615, "rel": [ "192.168.0.105_Tomcat-server" ] }, { "name": "pt_Windows 7", "type": "Windows 7", "x": 726.3267145730015, "y": 380.2458275232926, "rel": [] } ] } ] };
+var varConfig = {
+    "version": 5,
+    "datetime": "2016-06-20 17:29:23",
+    "monitorgroups": [{
+        "name": "Applications Manager",
+        "resourceid": "10000011",
+        "details": [{
+            "name": "192.168.0.105",
+            "type": "Port-Test",
+            "x": 753.1295220140064,
+            "y": 437.37489562537945,
+            "rel": []
+        }, {
+            "name": "192.168.0.105_PGSQL",
+            "type": "PostgreSQL",
+            "x": 578.4190304666614,
+            "y": 340.0288934307075,
+            "rel": ["pt_Windows 7", "192.168.0.105"]
+        }, {
+            "name": "192.168.0.105_Tomcat-server",
+            "type": "Tomcat-server",
+            "x": 378.42801078385446,
+            "y": 341.92416286566674,
+            "rel": ["192.168.0.105_PGSQL"]
+        }, {
+            "name": "AppManager Home Page",
+            "type": "UrlMonitor",
+            "x": 178.4409993258962,
+            "y": 339.64485417343746,
+            "rel": ["192.168.0.105_Tomcat-server"]
+        },
+            {"name": "pt_Windows 7", "type": "Windows 7", "x": 744.5933532759286, "y": 228.7360615012367, "rel": []}]
+    }]
+};
