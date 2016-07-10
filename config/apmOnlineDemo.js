114d2369d215b2c4f6bd09d1effe26d6f35768db
pengduo <wang@pengduo.com.cn>
2016-07-10 15:50:52 +0800

    增加了告警列表，发现Api取告警不完整，先完善了功能。

---
+++ config/apmOnlineDemo.js
@@ -0,0 +1,5 @@
+var apmHost = "demo.appmanager.com";
+var apmPort = "80";
+var apiKey = "fc165782b17a13c188e077866a03538e";  //PC机
+var strMoniterGroupName = 'Applications Manager'; //当前的业务组名称,这里不用修改，系统会从APM中自动获取
+var strResourceID = "10000012";//当前的业务组ID,这里不用修改，系统会从APM中自动获取
