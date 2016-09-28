f701be9111f83c0bce71b85d63afaa60f7173767
pengduo <wang@pengduo.com.cn>
2016-09-28 22:26:37 +0800

    分类化了响应时间

--- config/apm.js
+++ config/apm.js
@@ -1,5 +1,7 @@
-var apmHost = "localhost";
-var apmPort = "9090";
-var apiKey = "1240c50366c8d4b7777723ebd22ed80f";  //PC机
+//var apmHost = "localhost";
+var apmHost="demo.appmanager.com";
+var apmPort = "80";
+//var apiKey = "1240c50366c8d4b7777723ebd22ed80f";  //X1
+var apiKey="6ad9b7ea8760af7fe617b6293c55cadc";
 //var strMoniterGroupName = 'Applications Manager'; //当前的业务组名称,这里不用修改，系统会从APM中自动获取
 //var strResourceID = "10000012";//当前的业务组ID,这里不用修改，系统会从APM中自动获取
--- config/topo.js
+++ config/topo.js
@@ -1,65 +1,6 @@
 var varConfig = {
-    "version": 7,
+    "version": 1,
     "datetime": "2016-09-21 20:12:36",
-    "monitorgroups": [{
-        "name": "My group",
-        "resourceid": "10000095",
-        "details": [{
-            "name": "192.168.1.10_PGSQL",
-            "type": "PostgreSQL",
-            "x": 284.8616104146215,
-            "y": 439.7966754890194,
-            "rel": []
-        }, {
-            "name": "AppManager Home Page",
-            "type": "UrlMonitor",
-            "x": 651.4654035654058,
-            "y": 237.13065344694303,
-            "rel": ["192.168.1.10_PGSQL"]
-        }]
-    }, {
-        "name": "My App2",
-        "resourceid": "10000093",
-        "details": [{
-            "name": "192.168.1.10_PGSQL",
-            "type": "PostgreSQL",
-            "x": 484.3739694802238,
-            "y": 83.85268370713801,
-            "rel": ["192.168.1.10_Tomcat-server"]
-        }, {
-            "name": "192.168.1.10_Tomcat-server",
-            "type": "Tomcat-server",
-            "x": 309.3716049189908,
-            "y": 448.81822868450297,
-            "rel": []
-        }]
-    }, {
-        "name": "Applications Manager",
-        "resourceid": "10000011",
-        "details": [{
-            "name": "192.168.1.10",
-            "type": "Port-Test",
-            "x": 446.2675553903627,
-            "y": 77.62922107854084,
-            "rel": ["192.168.1.10_PGSQL", "192.168.1.10_Tomcat-server"]
-        }, {
-            "name": "192.168.1.10_PGSQL",
-            "type": "PostgreSQL",
-            "x": 167.28660143554342,
-            "y": 2.638496316207495,
-            "rel": []
-        }, {
-            "name": "192.168.1.10_Tomcat-server",
-            "type": "Tomcat-server",
-            "x": 153.20335435149534,
-            "y": 249.54770518870032,
-            "rel": []
-        }, {
-            "name": "AppManager Home Page",
-            "type": "UrlMonitor",
-            "x": 686.2760888224622,
-            "y": 103.4274094502341,
-            "rel": ["192.168.1.10"]
-        }]
-    }]
+    "monitorgroups": []
+
 };
