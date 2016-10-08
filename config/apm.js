76812253cdfd6340fdf70ae466f45e7dcd145dfc
pengduo <wang@pengduo.com.cn>
2016-10-08 23:42:15 +0800

    增加了只显示子组的功能，在apm.js文件中设置即可。

--- config/apm.js
+++ config/apm.js
@@ -1,7 +1,10 @@
-//var apmHost = "localhost";
-var apmHost="demo.appmanager.com";
-var apmPort = "80";
-//var apiKey = "1240c50366c8d4b7777723ebd22ed80f";  //X1
-var apiKey="6ad9b7ea8760af7fe617b6293c55cadc";
+var apmHost = "localhost";
+//var apmHost="demo.appmanager.com";
+var apmPort = "9090";
+var apiKey = "1240c50366c8d4b7777723ebd22ed80f";  //X1
+//var apiKey="6ad9b7ea8760af7fe617b6293c55cadc";
+var showSubGroupList="业务组名havesubgroup";
+var refreshInterval=5000; //定期刷新状态,MS
+
 //var strMoniterGroupName = 'Applications Manager'; //当前的业务组名称,这里不用修改，系统会从APM中自动获取
 //var strResourceID = "10000012";//当前的业务组ID,这里不用修改，系统会从APM中自动获取
--- config/topo.js
+++ config/topo.js
@@ -1,153 +1,99 @@
 var varConfig = {
-    "version": 30,
-    "datetime": "2016-09-30 9:31:58",
+    "version": 5,
+    "datetime": "2016-10-08 21:52:11",
     "monitorgroups": [{
+        "name": "业务组名havesubgroup",
+        "resourceid": "10000100",
+        "details": [{
+            "name": "子组名1",
+            "type": "Sub Group",
+            "x": 596.5783642604947,
+            "y": 149.56861822865903,
+            "rel": ["子组名2"]
+        }, {
+            "name": "子组名2",
+            "type": "Sub Group",
+            "x": 399.6768295764923,
+            "y": 384.27167907357216,
+            "rel": ["子组名3", "子组名4"]
+        }, {
+            "name": "子组名3",
+            "type": "Sub Group",
+            "x": 243.83947271853685,
+            "y": 370.58325936086476,
+            "rel": []
+        }, {"name": "子组名4", "type": "Sub Group", "x": 762.2761033475399, "y": 471.8738197814673, "rel": []}]
+    }, {
+        "name": "My App2",
+        "resourceid": "10000093",
+        "details": [{
+            "name": "192.168.1.10_PGSQL",
+            "type": "PostgreSQL",
+            "x": 11.348244734108448,
+            "y": 479.46844222024083,
+            "rel": ["192.168.1.10_Tomcat-server"]
+        }, {
+            "name": "192.168.1.10_Tomcat-server",
+            "type": "Tomcat-server",
+            "x": 661.6688327863812,
+            "y": 198.35002315230668,
+            "rel": []
+        }]
+    }, {
         "name": "Applications Manager",
         "resourceid": "10000011",
         "details": [{
-            "name": "172.21.199.125",
+            "name": "192.168.1.10",
             "type": "Port-Test",
-            "x": 187.03271597623825,
-            "y": 93.52386351674795,
-            "rel": ["172.21.199.125_Tomcat-server"]
+            "x": 491.979331150651,
+            "y": 488.7872581835836,
+            "rel": []
         }, {
-            "name": "172.21.199.125_Tomcat-server",
+            "name": "192.168.1.10_PGSQL",
+            "type": "PostgreSQL",
+            "x": 48.33866264671087,
+            "y": 244.9435567483306,
+            "rel": []
+        }, {
+            "name": "192.168.1.10_Tomcat-server",
             "type": "Tomcat-server",
-            "x": 299.1350354626775,
-            "y": 455.7921084109694,
-            "rel": ["CouchBase"]
+            "x": 375.35916939377785,
+            "y": 391.9087754096836,
+            "rel": ["192.168.1.10_PGSQL", "192.168.1.10"]
         }, {
             "name": "AppManager Home Page",
             "type": "UrlMonitor",
-            "x": 380.9271313250065,
-            "y": 121.640908671543,
-            "rel": ["172.21.199.125"]
-        }, {"name": "CouchBase", "type": "Couchbase", "x": 257.3898494243622, "y": 526.5655388813466, "rel": []}]
+            "x": 594.521052390337,
+            "y": 91.57137293368578,
+            "rel": ["192.168.1.10_Tomcat-server"]
+        }]
     }, {
-        "name": "Customer Portal",
-        "resourceid": "10001434",
+        "name": "My App6",
+        "resourceid": "10000098",
         "details": [{
-            "name": "Apache",
-            "type": "Apache-server",
-            "x": 96.46700657904148,
-            "y": 124.41251431591809,
+            "name": "192.168.1.10_Tomcat-server",
+            "type": "Tomcat-server",
+            "x": 731.4893160015345,
+            "y": 489.6390904672444,
+            "rel": ["192.168.1.10_PGSQL"]
+        }, {
+            "name": "AppManager Home Page",
+            "type": "UrlMonitor",
+            "x": 582.849327288568,
+            "y": 111.94908474572003,
+            "rel": ["192.168.1.10_Tomcat-server"]
+        }, {
+            "name": "192.168.1.10",
+            "type": "Port-Test",
+            "x": 34.70288272947073,
+            "y": 558.5492171812803,
             "rel": []
         }, {
-            "name": "ApacheServer",
-            "type": "Docker Container",
-            "x": 467.2455722466111,
-            "y": 237.47789952903986,
-            "rel": []
-        }, {
-            "name": "Ceph",
-            "type": "Ceph Storage",
-            "x": 94.90614365786314,
-            "y": 516.2488531786948,
-            "rel": ["ServiceBus"]
-        }, {
-            "name": "Hbase",
-            "type": "HBase",
-            "x": 217.418653331697,
-            "y": 31.247458700090647,
-            "rel": []
-        }, {
-            "name": "Novell",
-            "type": "Novell",
-            "x": 36.45944073796272,
-            "y": 306.37030242942274,
-            "rel": ["Ceph"]
-        }, {
-            "name": "ServiceBus",
-            "type": "AzureServiceBus",
-            "x": 174.50823038816452,
-            "y": 553.9393586106598,
-            "rel": ["hadoop", "Apache"]
-        }, {
-            "name": "Solr",
-            "type": "ApacheSolr",
-            "x": 541.1742422729731,
-            "y": 417.01376074925065,
-            "rel": ["ServiceBus"]
-        }, {
-            "name": "Webserver",
-            "type": "Apache-server",
-            "x": 354.61859088391066,
-            "y": 186.0326096881181,
-            "rel": []
-        }, {
-            "name": "apm-u12-lap1",
-            "type": "Node",
-            "x": 162.62428853660822,
-            "y": 108.12652562744915,
-            "rel": []
-        }, {
-            "name": "cassandra",
-            "type": "Cassandra",
-            "x": 140.4026748612523,
-            "y": 94.98943900689483,
-            "rel": []
-        }, {
-            "name": "hadoop",
-            "type": "Hadoop",
-            "x": 301.15902218967676,
-            "y": 92.67399902455509,
-            "rel": []
-        }, {"name": "informic", "type": "Informix", "x": 661.9129244238138, "y": 393.3343686629087, "rel": ["Solr"]}]
-    }, {
-        "name": "MyApplication",
-        "resourceid": "10001433",
-        "details": [{
-            "name": "EBS_11",
-            "type": "OracleEBS",
-            "x": 607.8357705846429,
-            "y": 17.809902923181653,
-            "rel": ["Nosql"]
-        }, {
-            "name": "Kafka_testSetup",
-            "type": "Kafka",
-            "x": 247.6796070113778,
-            "y": 501.61650334484875,
-            "rel": []
-        }, {
-            "name": "ListMonitorsXML",
-            "type": "RESTAPIMonitor",
-            "x": 331.09199590981007,
-            "y": 120.21829769946635,
-            "rel": []
-        }, {
-            "name": "Nosql",
-            "type": "OracleNoSQL",
-            "x": 529.2978750541806,
-            "y": 50.06734631024301,
-            "rel": ["tuxedo"]
-        }, {
-            "name": "Solr",
-            "type": "ApacheSolr",
-            "x": 412.9088142886758,
-            "y": 492.32170111499727,
-            "rel": ["ListMonitorsXML"]
-        }, {
-            "name": "postgres",
+            "name": "192.168.1.10_PGSQL",
             "type": "PostgreSQL",
-            "x": 182.39092640578747,
-            "y": 440.0439966470003,
-            "rel": []
-        }, {
-            "name": "tuxedo",
-            "type": "Oracle Tuxedo",
-            "x": 190.23768159002066,
-            "y": 29.698182502761483,
-            "rel": ["ListMonitorsXML"]
+            "x": 229.46697659790516,
+            "y": 366.5591633878648,
+            "rel": ["AppManager Home Page", "192.168.1.10"]
         }]
-    }, {
-        "name": "b5a30645-e91c-1eb3-fe7b-8effafc8dd0b",
-        "resourceid": "10000687",
-        "details": [{
-            "name": "172.21.10.45",
-            "type": "XenServerHost",
-            "x": 212.9290882498026,
-            "y": 516.8558409903198,
-            "rel": []
-        }]
-    }, {"name": "db", "resourceid": "10001441", "details": []}]
+    }]
 };
