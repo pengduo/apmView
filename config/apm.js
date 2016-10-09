dd62226af1c38b1b9eb93d027d0ff06a7d66f975
pengduo <wang@pengduo.com.cn>
2016-10-09 12:48:35 +0800

    需要显示子组的业务组名称，显示名称，可以多个，按如下格式放置多个new Array("Saab","Volvo","BMW")

--- config/apm.js
+++ config/apm.js
@@ -3,7 +3,7 @@ var apmHost = "localhost";
 var apmPort = "9090";
 var apiKey = "1240c50366c8d4b7777723ebd22ed80f";  //X1
 //var apiKey="6ad9b7ea8760af7fe617b6293c55cadc";
-var showSubGroupList="业务组名havesubgroup";
+var showSubGroupList=new Array("My group","业务组名havesubgroup"); //需要显示子组的业务组名称，显示名称，可以多个，按如下格式放置多个new Array("Saab","Volvo","BMW")
 var refreshInterval=5000; //定期刷新状态,MS
 
 //var strMoniterGroupName = 'Applications Manager'; //当前的业务组名称,这里不用修改，系统会从APM中自动获取
